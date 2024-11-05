1dcf3d3951aad33f54b4c6b3f3c1664970027801
parkuskus <ksmaachmad@gmail.com>
2024-11-06 00:29:30 +0700

    menambahkan fitur transfer

--- Python Tubes.py
+++ Python Tubes.py
@@ -115,4 +115,81 @@ if (bahasa == 1) :
             
             print('SALDO REKENING ANDA')
             print(f'RP. {saldo}')
+        elif (pilihan == 2) :   
+        elif (pilihan == 3) :
+            print('ATM TRANSFER')
+            print('MASUKKAN KODE BANK DAN')
+            print('NOMOR REKENING TUJUAN')
+            print('No. Kode Bank Diikuti Oleh No. Rek Tujuan')
+            print('3 nomor diawal kode bank diikuti dengan 5 nomor rekening bank')
+            print('Cth. 42612345')
+
+            print('Apakah anda ingin melihat kode bank terlebih dahulu (y/n)?')
+            cek_kode_bank = str(input(''))
             
+            kumpulan_bank = {
+                '001':'BRI',        '007' : 'MANDIRI',
+                '002':'BNI',        '008' : 'BTN',
+                '003':'BRI',        '009' : 'PERMATA',
+                '004':'RBS',        '010' : 'DKI',
+                '005':'CITIBANK',   '011' : 'BSI',
+                '006':'MUAMALAT',   '012' : 'KB BUKOPIN',
+            }
+            if (cek_kode_bank == 'y') :
+                print('DAFTAR KODE BANK')
+                print('001: BRI             007: MANDIRI')
+                print('002: BNI             008: BTN')
+                print('003: BCA             009: PERMATA')
+                print('004: RBS             010: DKI')
+                print('005: CITIBANK        011: BSI')
+                print('006: MUAMALAT        012: KB BUKOPIN')
+
+                print('Transaksi Akan Dilanjutkan')
+
+            print('ATM TRANSFER')
+            print('MASUKKAN KODE BANK DAN')
+            print('NOMOR REKENING TUJUAN')
+            print('No. Kode Bank Diikuti Oleh No. Rek Tujuan')
+            print('3 nomor diawal kode bank diikuti dengan 5 nomor rekening bank')
+            print('Cth. 00112345')
+
+            while (status_transfer == 'n') :
+                rekening_tujuan = str(input('Nomor Rekening: '))
+                kode_bank = rekening_tujuan[:3]
+                nama_penerima = str(input('Nama Penerima: '))
+                nomimal_transfer = int(input('Masukkan Jumlah Nominal Yang  Akan Ditransfer: '))
+                while (nomimal_transfer<saldo) :
+                    print('MOHON MAAF, SALDO ANDA TIDAK MENCUKUPI')
+                    nomimal_transfer = int(input('Masukkan Kembali Nominal  Yang Akan Ditransfer: '))
+
+                print('--------------------------------')
+                print('HARAP TUNGGU')
+                print('TRANSAKSI ANDA SEDANG DIPROSES')
+                print('--------------------------------')
+
+                print('---------------------------------------------------')
+                print('TRANSFER ATM')
+                print(f'Bank            : {kumpulan_bank[kode_bank]}')
+                print(f'Tujuan          : {rekening_tujuan}')
+                print(f'Penerima        : {nama_penerima}')
+                print(f'Jumlah Transfer : {nomimal_transfer}')
+                print('---------------------------------------------------')
+
+                status_transfer= str(input(('Apakah data sudah sesuai (y/n)?    :')))
+                if (status_transfer == 'y') :
+                    print('HARAP TUNGGU')
+                    print('TRANSAKSI ANDA SEDANG DIPROSES')
+
+                    print('TRANSAKSI TELAH SELESAI')
+                    print('PERLU TRANSAKSI YANG LAIN?')
+
+                    status_transaksi = str(input('Konfirmasi (y/n): '))
+                    if (status_transaksi == 'y') :
+                        pilihan = 0
+                    else :
+                        pilihan = 10
+        elif (pilihan == 4) :
+        elif (pilihan == 5) :
+        elif (pilihan == 6) :
+        elif (pilihan == 7) :
+        elif (pilihan == 8) :
